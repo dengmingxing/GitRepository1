8a0605fc7122ccdbe4d6a21adc213e268c5a060e
liyunfeng <liyunfeng@shenzhenpoly.com>
2019-05-09 18:13:23 +0800

    提交了简单的vue

---
+++ vue2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!--这是展示图片的，其中图片可以放到assets里或者static-->
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue2/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1>这是登录页面</h1>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
